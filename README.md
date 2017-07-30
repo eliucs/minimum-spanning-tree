6eee1bfbaf621d11840dfe3c0570c5a77d00a9f8
Eric Liu <ericliu314@gmail.com>
2017-07-29 21:56:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Minimum Spanning Tree Demonstration
+
+This is a demonstration in Node.js of Prim's algorithm and Kruskal's algorithm
+to find the minimum spanning tree of a set of vertices. Graphics use the
+Paper.js library.
+
+A [minimum spanning tree](https://en.wikipedia.org/wiki/Minimum_spanning_tree)
+is a subset of edges of a weighted undirected graph such that it connects
+all vertices but with the minimum combined edge weight.
+
+### Prim's algorithm
+
+
+### Kruskal's algorithm
+
+
+### Starting Up
+
+In the terminal:
+
+```
+npm start
+```
---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+// Minimum Spanning Tree
+// Demonstration of Prim's algorithm and Kruskal's algorithm to
+// compute the minimum spanning tree of a graph.
+// Source code: https://github.com/eliucs/minimum-spanning-tree
+// Website: http://ericliu.cs
+// Author: Eric Liu
+
+var path;
+
+var nodes = [];
+var nodeLayer = new Layer();
+var edgesLayer = new Layer();
+
+function onMouseDown(event) {
+  nodeLayer.activate();
+  path = new Path();
+  path.strokeColor = 'white';
+
+  var node = new Path.Circle({
+    center: event.point,
+    radius: 7
+  });
+
+  node.strokeColor = 'black';
+	node.fillColor = 'white';
+
+  nodes.push({
+    x: event.point.x,
+    y: event.point.y,
+    equals: function(that) {
+      return this.x == that.x && this.y == that.y
+    }
+  });
+
+  edgesLayer.activate();
+  edgesLayer.removeChildren();
+};
+
+$('#btn-reset').click(function() {
+  nodeLayer.removeChildren();
+  edgesLayer.removeChildren();
+  paper.view.draw();
+  nodes = [];
+});
